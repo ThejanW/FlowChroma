37db3805ecc9dfffd0b851c37a24a3db044f8f39
chamathabeysinghe <abeysinghechamath@gmail.com>
2018-07-07 17:00:55 +0530

    Inference using last frame

--- dataset/utils/inception_utils.py
+++ dataset/utils/inception_utils.py
@@ -5,12 +5,12 @@ from keras.applications.inception_resnet_v2 import preprocess_input
 import numpy as np
 import keras.backend as K
 
+K.clear_session()
 
+x = InceptionResNetV2(weights='imagenet', include_top=True)
+y = x.layers[-2].output
+model = Model(inputs=x.input, outputs=y)
 def inception_resnet_v2_predict(images):
-    K.clear_session()
     images = images.astype(np.float32)
-    x = InceptionResNetV2(weights='imagenet', include_top=True)
-    y = x.layers[-2].output
-    model = Model(inputs=x.input, outputs=y)
     predictions = model.predict(preprocess_input(images))
     return predictions
